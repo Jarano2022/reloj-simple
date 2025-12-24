75a4e4b80712d539b1f6bf1c9473c086ea698930
Jarano2022 <Jarano2023@gmail.com>
2025-12-24 17:57:45 +0100

    todo

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
+function diaDeLaSemana() {
+  const dias = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+  ];
+  const ahora = new Date();
+  return dias[ahora.getDay()];
+}
+
+function Mes() {
+  const meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+  const ahora = new Date();
+  return meses[ahora.getMonth()];
+}
+
 function actualizarReloj() {
-            const ahora = new Date();
-            const horas = String(ahora.getHours()).padStart(2, '0');
-            const minutos = String(ahora.getMinutes()).padStart(2, '0');
-            const segundos = String(ahora.getSeconds()).padStart(2, '0');
+  const ahora = new Date();
+  const horas = String(ahora.getHours()).padStart(2, "0");
+  const minutos = String(ahora.getMinutes()).padStart(2, "0");
+  const segundos = String(ahora.getSeconds()).padStart(2, "0");
+  const dia = String(ahora.getDate()).padStart(2, "0");
+  const diaSemana = diaDeLaSemana();
+  const mes = Mes();
+  const anio = ahora.getFullYear();
+  const fecha = ` ${diaSemana}, ${dia} de ${mes} de ${anio}`;
+  const reloj = `${horas}:${minutos}:${segundos}`;
 
-            document.getElementById('reloj').textContent =
-                `${horas}:${minutos}:${segundos}`;
-        }
+  document.getElementById("fecha").textContent = fecha;
 
-        setInterval(actualizarReloj, 1000);
-        actualizarReloj();
+  document.getElementById("reloj").textContent = reloj;
+}
+
+setInterval(actualizarReloj, 1000);
+actualizarReloj();
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'space') {
+  const fecha = document.getElementById('fecha').style.display;
+  if (fecha === 'none') {
+    document.getElementById('fecha').style.display = 'block';
+  } else {
+    document.getElementById('fecha').style.display = 'none';
+  }
+}
+});
